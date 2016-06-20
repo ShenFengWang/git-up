fead0869b4b7257cc0cf285204df9579bddb8d00
ShenFengWang <326826678@qq.com>
2016-06-21 00:14:37 +0800

    add functions

--- gitup.class.php
+++ gitup.class.php
@@ -1,11 +1,19 @@
 <?php
 class Gitup{
 
+    private $commitHash = array();
+
     private $config = array(
-    'REP_ROOT' => './',
+        'IDENTIFYCODE' => 'git-up',
+        'MODE' => 'post',
+        'REP_ROOT' => './',
+        'PUSH_PATH' => 'localhost',
+        'SERVER_ROOT' => __DIR__,
+        'POSTDATA_PATH' => __DIR__,
+        'ACCEPTDATA_PATH' => __DIR__,
     );
 
-    function __construct($config){
+    function __construct($config = null){
         if(is_array($config)){
             foreach($config as $key => $val){
                 $upperKey = strtoupper($key);
@@ -14,6 +22,92 @@ class Gitup{
                 }
             }
         }
+    }
+
+    public function config($key, $val = null){
+        if(isset($this->config[$key])){
+            if(is_null($val)){
+                return $this->config[$key];
+            }else{
+                $this->config[$key] = $val;
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    public function init(){
+        $this->initDir();
+        $this->config['IDENTIFYCODE'] = sha1($this->config['IDENTIFYCODE']);
+        if($this->config['MODE'] == 'post'){
+            if(file_exists($this->config['REP_ROOT'])){
+                if(!$this->getGitLog())throw new Exception('there is no information using "git log",please check the repository path!');
+            }else{
+                throw new Exception('No Such Directory:' . $this->config['REP_ROOT']);
+            }
+        }else{
+        
+        }
+    }
+
+    protected function initDir($key = null){
+        $dirParameter = array(
+        'REP_ROOT',
+        'SERVER_ROOT',
+        'POSTDATA_PATH',
+        'ACCEPTDATA_PATH',
+        );
+        if(is_null($key)){
+            foreach($urlParameter as $val){
+                $this->config[$val] = rtrim($this->config[$val], '/') . '/';
+            }
+            return true;
+        }else{
+            $key = strtoupper($key);
+            if(in_array($key,$urlParameter)){
+                $this->config[$key] = rtrim($this->config[$key], '/') . '/';
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    protected function getGitLog(){
+        exec("git -C {$this->config['REP_ROOT']} log",$execResult);
+        if(empty($execResult))return false;
+        foreach($execResult as $key => $val){
+            if(strpos($val,'commit ') === 0){
+                $commitStr = explode(' ',$val)[1];
+                preg_match('/^[a-z\d]{40}$/',$commitStr) and $this->commitHash[] = $commitStr;
+            }
+        }
+        return count($this->commitHash) ? : false;
+    }
+
+    public function checkServer(){
+        $postData = array('a' => 'a');
+        return $this->curlToServer($postData);
+    }
+
+    private function curlToServer(array $data,$file = null){
+        $ch = curl_init();
+        is_null($file) or $data['upload_file'] = new CURLFile($file);//required PHP version >= 5.5
+        $options = array(
+            CURLOPT_URL => $this->config['PUSH_PATH'],
+            CURLOPT_SAFE_UPLOAD => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_TIMEOUT => 30,
+        );
+        curl_setopt_array($ch, $options);
+        $curlResult = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return $httpCode == '200' ? $curlResult : false;
     }
 
 }
--- post.php
+++ post.php
@@ -1,32 +1,21 @@
 <?php
 
 //class file
-define('CLASS_PATH','');
+define('CLASS_PATH','.');
 
 include rtrim(CLASS_PATH, '/') . '/' . 'gitup.class.php';
 $config = array(
     'rep_root' => '/home/wang/doc/linux_setting/',
+    'push_path' => 'http://git-up/accept.php',
 );
-$gitup = new Gitup();
+$gitup = new Gitup($config);
 
 try{
-
+    var_dump($gitup->checkServer());
 }catch(Exception $e){
     print $e->getMessage();
 }
 
-$execDir = REP_ROOT ? : './';
-exec("git -C {$execDir} log",$execResult);
-if(empty($execResult))exit("no log for git");
-
-//handle result
-foreach($execResult as $key => $val){
-    if(strpos($val,'commit ') === 0){
-        $gitCommitHash = explode(' ',$val)[1];
-        preg_match('/^[a-z\d]{40}$/',$gitCommitHash) and $hashArray[] = $gitCommitHash;
-    }
-}
-var_dump($hashArray);
 
 
 ?>
